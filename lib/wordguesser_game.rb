3d50fad791e1a47df1cb67ad94e67fdfb68a8ae8
Vibha Dabholkar <vibha.dabholkar@berkeley.edu>
2024-09-23 08:07:51 +0000

    updates

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -5,8 +5,53 @@ class WordGuesserGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess_several_letters(letters)
+    for letter in letters.chars
+      guess_letter(letter)
+    end
+  end
+
+  def guess(letter)
+    if letter.nil? || letter == '' || !letter.match?(/[a-zA-Z]/)
+      raise ArgumentError.new('Invalid guess. Please enter a letter.')
+    end
+
+    letter = letter.downcase
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+    if @word.include?(letter)
+      @guesses += letter unless @guesses.include?(letter)
+    else
+      @wrong_guesses += letter unless @wrong_guesses.include?(letter)
+    end
+    true
+  end
+
+  def word_with_guesses
+    result = ''
+    for c in @word.chars
+      result += @guesses.include?(c) ? c : '-'
+    end
+    result
+  end
+
+  def check_win_or_lose
+    if @word == word_with_guesses
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
